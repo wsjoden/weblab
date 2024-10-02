403d96ca6833fb312423d8aba455c5e60decf1c9
wsjoden <sjoden.william@gmail.com>
2024-10-02 13:40:56 +0200

    can submit itemID to ItemHandler

--- src/main/java/bo/ItemHandler.java
+++ src/main/java/bo/ItemHandler.java
@@ -1,20 +1,27 @@
 package bo;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import ui.ItemInfo;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
-public class ItemHandler {
+public class ItemHandler extends HttpServlet {
     public static Collection<ItemInfo> getItems(){
         Collection c = Item.getItems();
         ArrayList<ItemInfo> items = new ArrayList<ItemInfo>();
         for (Iterator it = c.iterator(); it.hasNext();) {
             Item item = (Item) it.next();
-            items.add(new ItemInfo(item.getName(),item.getDescription()));
-
+            items.add(new ItemInfo(item.getName(),item.getDescription(),item.getId()));
         }
         return items;
+    }
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp){
+        int id = Integer.parseInt(req.getParameter("itemId"));
+        System.out.println("added to cart: " + id);
     }
 }
